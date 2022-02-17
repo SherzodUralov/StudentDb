3e4d251c7d6d38f3dfaf3b6d8078a0fd117e07df
Sherzod9808 <uralovsherzod03@gmail.com>
2022-02-17 14:13:10 +0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentDb.DataAccseess.Models;
+using StudentDb.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentDb.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly IStudentRepository studentRepository;
+
+        //public HomeController(IStudentRepository studentRepository)
+        //{
+        //    this.studentRepository = studentRepository;
+        //}
+
+        public ViewResult Index() 
+        {
+            HomeDetalisViewModel homeDetalisView1 = new HomeDetalisViewModel()
+            {
+                Title = "Studen Index"
+            };
+            return View(homeDetalisView1);
+        }
+        public JsonResult Data() 
+        {
+            return Json(new {id=1,name="Sherzod",lastname="Uralov" });
+        }
+
+    }
+    
+}
---
+++ ViewModels/HomeDetalisViewModel.cs
@@ -0,0 +1,14 @@
+﻿using StudentDb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentDb.ViewModels
+{
+    public class HomeDetalisViewModel
+    {
+        public IEnumerable<Student> student { get; set; }
+        public string Title { get; set; }
+    }
+}
